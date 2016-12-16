15e9a883b7bfb1dbd34fcb39ec7c266d1f54cb20
Murph Grainger <murph.grainger@gmail.com>
2016-12-16 16:38:58 -0700

    solved promise issue

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ $(document).ready(function() {
     const cleanQuery = queryParse(window.location.search);
     getUserInfo(cleanQuery.id)
         .then(addUserInfoToPage)
-        .catch(errorFunction)
+        .catch(errorFunction);
 });
 
 function queryParse(query) {
-    let obj = {}
+    let obj = {};
     let arr1 = query.substr(1).split('&').forEach((element) => {
         let arr2 = element.split('=');
         obj[arr2[0]] = arr2[1];
-    })
+    });
     return obj;
 }
 
 function getUserInfo(id) {
-    $.get(`${API_URL}/user/${id}`)
+    console.log(id);
+    return $.get(`http://localhost:3000/user/${id}`);
 }
 
 function addUserInfoToPage(user) {
+    console.log(user);
     let source = $('#user-template').html();
     let template = Handlebars.compile(source);
     let context = user;
@@ -31,13 +33,12 @@ function addUserInfoToPage(user) {
 
 function getURL() {
     if (window.location.host.indexOf('localhost') != -1) {
-        console.log(window.location.host);
-        return 'http://localhost:3000'
+        return 'http://localhost:3000';
     } else {
-        return 'https://cruddydegree.herokuapp.com'
+        return 'https://cruddydegree.herokuapp.com';
     }
 }
 
 function errorFunction() {
-    alert('An Error Occured')
+    alert('An Error Occured');
 }

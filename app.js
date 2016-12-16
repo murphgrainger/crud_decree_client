c3964bc4660ebacc60ebfd8c3efbe7ca424f35d6
Murph Grainger <murph.grainger@gmail.com>
2016-12-16 16:52:49 -0700

    all functions run

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     const cleanQuery = queryParse(window.location.search);
     getUserInfo(cleanQuery.id)
         .then(addUserInfoToPage)
+        .then(getUserDegrees)
+        .then(addUserDegrees)
         .catch(errorFunction);
 });
 
@@ -21,6 +23,11 @@ function getUserInfo(id) {
     return $.get(`http://localhost:3000/user/${id}`);
 }
 
+function getUserDegrees(id) {
+    console.log(id);
+    return $.get(`http://localhost:3000/user/${id}/degree`);
+}
+
 function addUserInfoToPage(user) {
     console.log(user);
     let source = $('#user-template').html();
@@ -29,6 +36,17 @@ function addUserInfoToPage(user) {
     let html = template(context);
     $('.user-section').html(html);
     return user.id;
+}
+
+function addUserDegrees(degree) {
+    console.log(degree);
+    let source = $('#degree-template').html();
+    let template = Handlebars.compile(source);
+    let context = {
+        degree
+    };
+    let html = template(context);
+    $('.degree-section').html(html);
 }
 
 function getURL() {

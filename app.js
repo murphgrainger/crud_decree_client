568773b1309b02f976e217aa016fb7ea27cda5ba
Murph Grainger <murph.grainger@gmail.com>
2016-12-19 08:54:48 -0700

    fixed yet another heroku error

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function getURL() {
     if (window.location.host.indexOf('localhost') != -1) {
         return 'http://localhost:3000';
     } else {
-        return 'https://cruddydegree.herokuapp.com/';
+        return 'https://cruddydegree.herokuapp.com';
     }
 }
 
